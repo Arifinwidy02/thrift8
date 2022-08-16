55f2da72d1c8b9bb076cc64f92ea601174e2b387
Arifinwidy02 <arifinwidy@gmail.com>
2022-08-17 06:43:27 +0700

    tambahan route

--- route/route.js
+++ route/route.js
@@ -1,7 +1,9 @@
 const route = require('express').Router()
 
-route.get("/") //landing page
-route.get("/product") //
+route.get("/") //landing page 1.
+//regsiter 2.
+//login 3.
+route.get("/product") // 4. --> 
 // ngurangin produc
 //nambahin product
 // delete product
