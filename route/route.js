8fb39efa73e4c028bd32f5b746e6bc905eae0d0b
Arifinwidy02 <arifinwidy@gmail.com>
2022-08-16 21:13:04 +0700

    setup

---
+++ route/route.js
@@ -0,0 +1,10 @@
+const route = require('express').Router()
+
+route.get("/") //landing page
+route.get("/product") //
+// ngurangin produc
+//nambahin product
+// delete product
+//
+
+module.export = route
