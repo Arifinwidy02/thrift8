6c39e0e2a13af182035002feb1ba0cab3bc5d91c
Arifinwidy02 <arifinwidy@gmail.com>
2022-08-17 11:01:11 +0700

    session

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const app = express()
 const route = require('./route/route')
+const session = require('express-session')
 const port = 3000
 
 app.set("view engine", "ejs")
 app.use(express.urlencoded({extended:true}))
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false, //untuk login session
+  cookie: { 
+    secure: false, //untuk development, true untuk production
+    sameSite: true // tambahan aja untuk security
+  } 
+}))
 app.use(route)
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
